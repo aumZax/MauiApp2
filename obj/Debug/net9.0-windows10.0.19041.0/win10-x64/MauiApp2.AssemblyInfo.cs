8903084b2972763cdeda674729dd4fa78905a7cb
aumZax <oodgame.1h@gmail.com>
2025-03-28 12:44:43 +0700

    Mauiiiiiiiiiiii

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1925121f964b0bb522aa9cb7d970d3d6587ca189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1b75a5cd8ddbdd3af038ec529056a9c6300f70b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
